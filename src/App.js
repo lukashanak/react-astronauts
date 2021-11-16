de1389d770d5907a4f0b8c823b5e90f19aeae080
Lukáš Hanák <lukas.hanak@protonmail.com>
2021-11-16 18:55:41 +0100

    Creted table (html, css)

--- src/App.js
+++ src/App.js
@@ -7,8 +7,29 @@ const App = () => {
   const [astronauts, setAstronauts] = useState(data)
 
   return (
-    <div className="App">
-
+    <div className="App container">
+      <form>
+        <table className="table">
+          <thead className="thead">
+          <tr>
+              <th className="table__cell"><i className="fas fa-sort-down"></i>First name</th>
+              <th className="table__cell"><i className="fas fa-sort-down"></i>Second name</th>
+              <th className="table__cell"><i className="fas fa-sort-down"></i>Birthday</th>
+              <th className="table__cell"><i className="fas fa-sort-down"></i>Superpowers</th>
+              <th className="table__cell">Actions</th>
+            </tr>
+          </thead>
+          <tbody className>
+            <tr className="table__row">
+              <td className="table__cell">Random</td>
+              <td className="table__cell">Random</td>
+              <td className="table__cell">Random</td>
+              <td className="table__cell">Random</td>
+              <td className="table__cell">Random</td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     </div>
   );
 }
