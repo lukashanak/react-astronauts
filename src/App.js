c9b8be6d99e07f9ad7ce72519b7130ba4499c1d7
Lukáš Hanák <lukas.hanak@protonmail.com>
2021-11-16 19:40:02 +0100

    Built static version of AddForm

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
 
   return (
     <div className="App container">
-      <form>
+      <form className="margin-bottom-big">
         <table className="table">
           <thead className="thead">
           <tr>
@@ -32,6 +32,48 @@ const App = () => {
             ))}
           </tbody>
         </table>
+      </form>
+      <h2 className="text-center">Add new item</h2>
+      <form className="add__form">
+        <table className="table margin-bottom-small">
+        <tr className="table__row">
+            <td className="table__cell table__cell--edit">
+                <input
+                 type="text"
+                 required="required"
+                 name="firstName"
+                 placeholder="Enter the first name"
+                ></input>
+            </td>
+            <td className="table__cell table__cell--edit">
+                <input
+                 type="text"
+                 required="required"
+                 name="secondName"
+                 placeholder="Enter the second name"
+                ></input>
+            </td>
+            <td className="table__cell table__cell--edit">
+                <input
+                 type="text"
+                 required="required"
+                 name="birthday"
+                 placeholder="Enter birthday"
+                ></input>
+            </td>
+            <td className="table__cell table__cell--edit">
+                <input
+                 type="text"
+                 required="required"
+                 name="superpowers"
+                 placeholder="Enter superpowers"
+                ></input>
+            </td>
+        </tr>
+        </table>
+        <div className="btn-wrapper">
+          <button className="btn btn--long btn--primary">Add astronaut</button> 
+        </div>
       </form>
     </div>
   );
